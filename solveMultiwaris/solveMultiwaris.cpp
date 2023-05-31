7aa7f7b8678a7ec288426fcbd9a5fa0392c1671b
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-31 16:36:59 +0700

    menambahkan fungsi turunan dari class pekerja

--- solveMultiwaris/solveMultiwaris.cpp
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -11,3 +11,13 @@ public:
 		cout << "orang dibuat dengan umur " << umur << "\n" << endl;
 	}
 };
+
+class pekerja : virtual public orang {
+public:
+
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
