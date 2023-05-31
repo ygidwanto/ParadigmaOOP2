6f208c0d9ea67d458ce41d5422d4ed15cc9dd26d
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-05-31 17:12:56 +0700

    mematikan fungsi turunan menggunakan final

--- Program noOverriding/Program noOverriding.cpp
+++ Program noOverriding/Program noOverriding.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 class baseClass {
 public:
-	virtual void perkenalan() {
+	virtual void perkenalan() final {
 		cout << "Hallo saya Function dari base class";
+	}
+};
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
 	}
 };
